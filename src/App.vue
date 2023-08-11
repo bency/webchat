33cc3e71d1d9d38289a4c7c65bae4e34b4cab689
bency <bencylin@mood.day>
2023-08-11 23:40:18 +0800

    feat: 建立 uuid

--- src/App.vue
+++ src/App.vue
@@ -33,6 +33,29 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  setup: () => {
+    function _uuid() {
+      let uuid = window.localStorage.getItem('uuid');
+      if (uuid) {
+        return uuid;
+      }
+      let d = Date.now();
+      if (typeof performance !== 'undefined' && typeof performance.now === 'function') {
+        d += performance.now(); //use high-precision timer if available
+      }
+      uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        let r = (d + Math.random() * 16) % 16 | 0;
+        d = Math.floor(d / 16);
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+      });
+      window.localStorage.setItem('uuid', uuid);
+      return uuid;
+    }
+    return {
+      _uuid,
+      database
+    }
   }
 }
 </script>
