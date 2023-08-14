b98ee3031067d456a135af6f434cd63361259af2
bency <bencylin@mood.day>
2023-08-15 01:26:24 +0800

    feat: 重新命名 uuid

--- src/App.vue
+++ src/App.vue
@@ -35,26 +35,32 @@ export default {
     HelloWorld
   },
   setup: () => {
-    function _uuid() {
-      let uuid = window.localStorage.getItem('uuid');
+    function uuid() {
+      let uuid = window.localStorage.getItem("uuid");
       if (uuid) {
         return uuid;
       }
       let d = Date.now();
-      if (typeof performance !== 'undefined' && typeof performance.now === 'function') {
+      if (
+        typeof performance !== "undefined" &&
+        typeof performance.now === "function"
+      ) {
         d += performance.now(); //use high-precision timer if available
       }
-      uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-        let r = (d + Math.random() * 16) % 16 | 0;
-        d = Math.floor(d / 16);
-        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
-      });
-      window.localStorage.setItem('uuid', uuid);
+      uuid = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
+        /[xy]/g,
+        function (c) {
+          let r = (d + Math.random() * 16) % 16 | 0;
+          d = Math.floor(d / 16);
+          return (c === "x" ? r : (r & 0x3) | 0x8).toString(16);
+        }
+      );
+      window.localStorage.setItem("uuid", uuid);
       return uuid;
     }
     return {
-      _uuid,
-      database
+      uuid,
+      joinLivingRoom,
     }
   }
 }
