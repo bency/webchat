48335f4634e2a8b6948b6d379502791bc19c417b
bency <bencylin@mood.day>
2023-08-15 23:21:49 +0800

    feat: 由 helper 處理主要邏輯

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,42 @@
 <template>
+  <div class="container">
+    <div class="row">
+      <div class="col-4 bg-info solid-height">
+        線上人數：{{ onlineCount }}
+      </div>
+      <div class="col-8 bg-info">
+        <div class="row solid-height"></div>
+        <div class="row">
+          <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="哈囉" aria-label="Recipient's username"
+              aria-describedby="button-addon2">
+            <button class="btn btn-outline-secondary" type="button" id="button-addon2">送出</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
   <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
-  {{ uuid() }}
-  <button class="btn btn-primary" @click="startChat()">開始聊天</button>
 </template>
 
 <script setup>
 import { onMounted } from 'vue';
 // import HelloWorld from './components/HelloWorld.vue'
-import { uuid, startChat } from './components/TheHelpers.js'
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getDatabase, ref, update, onValue } from "firebase/database";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCUpev4Wf4yX9mms6_W9SAFKIA0Z3NH2IY",
-  authDomain: "webchat-337d6.firebaseapp.com",
-  projectId: "webchat-337d6",
-  storageBucket: "webchat-337d6.appspot.com",
-  messagingSenderId: "226537379811",
-  appId: "1:226537379811:web:493c70d363b9d5a3a8f2d6",
-  databaseURL: "https://webchat-337d6-default-rtdb.asia-southeast1.firebasedatabase.app/",
-  measurementId: "G-4M1R71FK77"
-};
-const app = initializeApp(firebaseConfig);
-
-// Initialize Firebase
-
-const joinLivingRoom = () => {
-  const uuid = uuid();
-  const db = getDatabase(app);
-  const path = `livingroom${uuid}`;
-  const updates = {};
-  updates['online_at'] = (new Date()).getTime();
-  updates['is_online'] = true;
-  update(ref(db, path), updates);
-}
+import { monitorLivingRoom, joinLivingRoom, userUid } from './components/TheHelpers.js'
+let onlineCount = 0;
 
 onMounted(() => {
-  const db = getDatabase(app);
-  joinLivingRoom();
-  onValue(ref(db, '/livingroom'), (snapshot) => {
-    const data = snapshot.val();
-    console.log('data', Array.prototype.keys(data));
-  });
+  joinLivingRoom(userUid());
+  monitorLivingRoom();
 });
 </script>
 
 <style>
+.solid-height {
+  min-height: 600px;
+  overflow-y: auto;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/components/TheHelpers.js
+++ src/components/TheHelpers.js
@@ -1,9 +1,35 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, update, onValue } from "firebase/database";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCUpev4Wf4yX9mms6_W9SAFKIA0Z3NH2IY",
+  authDomain: "webchat-337d6.firebaseapp.com",
+  projectId: "webchat-337d6",
+  storageBucket: "webchat-337d6.appspot.com",
+  messagingSenderId: "226537379811",
+  appId: "1:226537379811:web:493c70d363b9d5a3a8f2d6",
+  databaseURL:
+    "https://webchat-337d6-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  measurementId: "G-4M1R71FK77",
+};
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+export function getDb() {
+  return db;
+}
+
+export function userUid() {
+  let uuid = window.localStorage.getItem("userUid") || uuid();
+  window.localStorage.setItem("userUid", uuid);
+  return uuid;
+}
+
 export function uuid() {
-  let uuid = window.localStorage.getItem("uuid");
-  if (uuid) {
-    return uuid;
-  }
-  let d = Date.now();
+  let d = Date.now(),
+    uuid = null;
   if (
     typeof performance !== "undefined" &&
     typeof performance.now === "function"
@@ -19,4 +45,22 @@ export function uuid() {
   return uuid;
 }
 
-export function startChat() {}
+export function startChat() {
+  console.log("click");
+}
+
+export function joinLivingRoom(uuid) {
+  const db = getDatabase(app);
+  const path = `livingroom/${uuid}`;
+  const updates = {};
+  updates["online_at"] = new Date().getTime();
+  updates["is_online"] = true;
+  update(ref(db, path), updates);
+}
+
+export function monitorLivingRoom() {
+  onValue(ref(db, "/livingroom"), (snapshot) => {
+    const data = snapshot.val();
+    console.log("data", Object.keys(data));
+  });
+}
