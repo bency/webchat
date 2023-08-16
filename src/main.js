be43203910c514ee738e7aac861d372a2798fac8
roberthuang31759 <100658607+roberthuang31759@users.noreply.github.com>
2023-08-16 09:50:19 +0800

    fix: import pinia 順序問題

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import pinia from './pinia/store';
 import App from './App.vue'
-
-const pinia = createPinia()
 
 createApp(App).use(pinia).mount('#app')
