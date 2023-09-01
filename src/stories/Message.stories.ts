0de19010f15878db6a6183f5a7963f725eb68340
roberthuang31759 <100658607+roberthuang31759@users.noreply.github.com>
2023-09-01 11:14:20 +0800

    feat: 初始化 storybook 且新增 Message component

---
+++ src/stories/Message.stories.ts
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Message } from './Message';
+
+const meta: Meta<typeof Message> = {
+    component: Message,
+}
+
+export default meta;
+
+type Story = StoryObj<typeof Message>;
+
+export const Default: Story = {
+    args: {
+        message: 'Hello World!',
+    },
+};
