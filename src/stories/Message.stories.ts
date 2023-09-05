2263fab3013f6e5bc35b5fe4a64ddcedc000945e
roberthuang31759 <100658607+roberthuang31759@users.noreply.github.com>
2023-09-05 17:56:43 +0800

    feat: 訊息加入網址情況

--- src/stories/Message.stories.ts
+++ src/stories/Message.stories.ts
@@ -22,3 +22,9 @@ export const Partner: Story = {
         message: 'Hello World!',
     },
 }
+
+export const IncludeAUrlInTheMessage: Story = {
+    args: {
+        message: '速查、易買、好逛請使用 https://apps.apple.com/tw/app/mood-%E8%BF%BD%E8%B9%A4%E7%86%B1%E9%96%80%E8%A9%B1%E9%A1%8C-%E5%8C%BF%E5%90%8D%E7%99%BC%E5%95%8F%E8%A7%A3%E7%AD%94/id1616095362',
+    },
+}
